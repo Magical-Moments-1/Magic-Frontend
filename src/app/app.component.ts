617b53e516bee5ce1729fe9d66a8a1de423d4416
SaraRuthSofer <165088243+SaraRuthSofer@users.noreply.github.com>
2024-09-18 15:43:08 +0300

    Fix google login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { LoginComponent } from './components/login/login.component';
 import { FAQsComponent } from './Components/faqs/faqs.component';
+import { LoginComponent } from './Components/login/login.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,LoginComponent],
-  imports: [RouterOutlet ,FAQsComponent ],
+  imports: [RouterOutlet,LoginComponent, FAQsComponent],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
+import { LoginComponent } from './Components/login/login.component';
+import { GoogleLoginComponent } from './Components/google-login/google-login.component';
 
 export const routes: Routes = [
     { path: 'login', component: LoginComponent },
-    { path: '', component: LoginComponent },
+    { path: 'con', component: GoogleLoginComponent },
 
     { path: 'question', loadComponent: () => import('./Components/faqs/faqs.component').then(q=>q.FAQsComponent)},
     { path: 'register', loadComponent: () => import('./Components/register/register.component').then(r=>r.RegisterComponent)},

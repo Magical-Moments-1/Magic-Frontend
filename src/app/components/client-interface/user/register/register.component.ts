4af346c235d7aacbfa6521420cb5b4770bd57406
SaraRuthSofer <165088243+SaraRuthSofer@users.noreply.github.com>
2024-12-09 12:29:58 +0200

    z

--- src/app/components/client-interface/user/register/register.component.ts
+++ src/app/components/client-interface/user/register/register.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { UserService } from '../../../../services/user/user.service';
+import { UserService } from '../../../../‏‏services/user/user.service';
 
 @Component({
   selector: 'app-register',
